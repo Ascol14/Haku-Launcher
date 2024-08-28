d78d438593368e32aeb23d657690772a3bf435d6
Constant RUSCHE <constant.rusche@gmail.com>
2024-08-28 18:51:07 +0200

    Change theme

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,8 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Palanarchy =/= pas de vie", "author": "Ascol" },
+            { "message": "J'aime le code.", "author": "Ascol" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
